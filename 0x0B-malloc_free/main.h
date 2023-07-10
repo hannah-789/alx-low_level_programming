1632c7680cc06808a2c1a88d3bbc50a49cd0bcd7
hanae <hanaeelyaagoubi45@gmail.com>
2023-07-10 10:03:27 -0700

    updated main file

--- 0x0B-malloc_free/main.h
+++ 0x0B-malloc_free/main.h
@@ -1,5 +1,13 @@
 #ifndef MAIN_H
 #define MAIN_H
+#include <stdio.h>
+#include<stdlib.h>
+/**
+ * file: main.h
+ *  Desc: Header file containing prototypes for all functions
+ *       used in the 0x0B-malloc_free directory
+ */
+int _putchar(char C);
 char *create_array(unsigned int size, char c);
 char *_strdup(char *str);
 char *str_concat(char *s1, char *s2);
