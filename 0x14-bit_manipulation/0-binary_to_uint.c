cc6e9e6210b1fcf254159a98277a6e39d801b9b3
HANAE <hanaeelyaagoubi45@gmail.com>
2023-08-31 12:20:58 -0700

    this one

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -8,17 +8,23 @@
 */
 unsigned int binary_to_uint(const char *b)
 {
-    if (b == NULL)
-        return 0;
+	unsigned int total, power;
+	int len;
 
-    unsigned char power_of_2 = 1;
-    unsigned int total = 0;
+	if (b == NULL)
+		return (0);
 
-    for (int i = 0; b[i] != '\0'; i++)
-    {
-        total += power_of_2 * (b[i] - '0');
-        power_of_2 *= 2;
-    }
+	for (len = 0; b[len]; len++)
+	{
+		if (b[len] != '0' && b[len] != '1')
+			return (0);
+	}
 
-    return total;
+	for (power = 1, total = 0, len--; len >= 0; len--, power *= 2)
+	{
+		if (b[len] == '1')
+			total += power;
+	}
+
+	return (total);
 }
